b7d15fbc489b709a3bcae95f33cce45c5133a80c
Alex Strugacz <90818590+Nectar0@users.noreply.github.com>
2023-01-16 22:48:20 -0600

    added firebase, production build

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import firebase from 'firebase/compat/app';
 
+const config = {
+    apiKey: "AIzaSyD6VismmzfvkpwSJgEGbvWv2dPakCD06Qo",
+    authDomain: "alexweb-1.firebaseapp.com",
+    projectId: "alexweb-1",
+    storageBucket: "alexweb-1.appspot.com",
+    messagingSenderId: "370821814556",
+    appId: "1:370821814556:web:c8832981240b2f21cc82d9",
+    measurementId: "G-HVT803NFSG"
+};
+
+firebase.initializeApp(config);
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
